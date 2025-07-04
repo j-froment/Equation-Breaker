390486f62205d6d5ca82d869f27305eb375242ef
Vyshmahi <vyshmahi@Vyshnavis-MacBook-Air.local>
2025-07-03 22:30:52 -0400

    OCR added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,10 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_application_2"
+     namespace = "com.example.ocrtestapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
